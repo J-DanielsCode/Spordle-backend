1a3f7311e053218a135a9ed121f0f8f429f03105
Jordan_D <jordanholstdaniels@gmail.com>
2025-11-07 17:45:38 +0000

    imported api gateway and dynamodb

--- import.tf
+++ import.tf
@@ -1,7 +1,16 @@
 resource "aws_api_gateway_rest_api" "my_api" {
-  name = "nba_rest_api_gateway"
+  name = "NBA_data_api"
 }
 
 resource "aws_dynamodb_table" "my_table" {
-  name = "nba_player_data_table"
+    name         = "nba-player-data"
+    billing_mode = "PAY_PER_REQUEST"
+
+    hash_key     = "player_id"
+
+    attribute {
+        name = "player_id"
+        type = "N"
+    }
 }
+
