a822d2432d18bb5daa8f8c61c09932f97f88a90b
Jordan_D <jordanholstdaniels@gmail.com>
2025-11-09 00:36:30 +0000

    created lambda fun, linked to api gateway and cloud and permissions

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,46 +1,131 @@
 # -- Resources --
 resource "aws_api_gateway_resource" "items" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.qdvsgwpzr0
-  parent_id = aws_api_gateway_rest_api.my_api.wa6t37r5u8
-  path_part = "lambda"
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_rest_api.my_api.root_resource_id
+  path_part   = "lambda"
 }
 
 resource "aws_api_gateway_resource" "item" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.qdvsgwpzr0
-  resource_id = aws_api_gateway_resource.items.g6cege
-  path_part = "{id}"
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  parent_id   = aws_api_gateway_resource.items.id
+  path_part   = "{id}"
 }
 
-#-- GET all --
+# -- GET all (/lambda) --
 resource "aws_api_gateway_method" "get_all" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.qdvsgwpzr0
-  resource_id = aws_api_gateway_resource.items.eqrnch
-  http_method = "GET"
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "get_all" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.qdvsgwpzr0
-  resource_id             = aws_api_gateway_resource.items.eqrnch
-  http_method             = aws_api_gateway_method.get_one_method.http_method
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.get_all.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_lambda.invoke_arn
+  uri                     = aws_lambda_function.nba_api_lambda.invoke_arn
 }
 
-#-- GET one --
-resource "aws_api_gateway_resource" "get_one" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.qdvsgwpzr0
-  resource_id   = aws_api_gateway_resource.items.g6cege
-  http_method = "GET"
+# -- POST (/lambda) --
+resource "aws_api_gateway_method" "post_item" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.items.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_item" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.items.id
+  http_method             = aws_api_gateway_method.post_item.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.nba_api_lambda.invoke_arn
+}
+
+# -- GET one (/lambda/{id}) --
+resource "aws_api_gateway_method" "get_one" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.item.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "get_one" {
-  rest_api_id             = aws_api_gateway_rest_api.my_api.qdvsgwpzr0
-  resource_id             = aws_api_gateway_resource.lambda_id_resource.g6cege
-  http_method             = aws_api_gateway_method.get_one_method.http_method
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.item.id
+  http_method             = aws_api_gateway_method.get_one.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_lambda.invoke_arn
+  uri                     = aws_lambda_function.nba_api_lambda.invoke_arn
+}
+
+# -- DELETE (/lambda/{id}) --
+resource "aws_api_gateway_method" "delete_item" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.item.id
+  http_method   = "DELETE"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "delete_item" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.item.id
+  http_method             = aws_api_gateway_method.delete_item.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.nba_api_lambda.invoke_arn
+}
+
+# -- PATCH (/lambda/{id}) --
+resource "aws_api_gateway_method" "patch_item" {
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  resource_id   = aws_api_gateway_resource.item.id
+  http_method   = "PATCH"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "patch_item" {
+  rest_api_id             = aws_api_gateway_rest_api.my_api.id
+  resource_id             = aws_api_gateway_resource.item.id
+  http_method             = aws_api_gateway_method.patch_item.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.nba_api_lambda.invoke_arn
+}
+
+# -- Lambda permission --
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.nba_api_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.my_api.execution_arn}/*/*"
+}
+
+# --api gateway deployment--
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+
+  # Forces redeployment when methods or integrations change
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.my_api))
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.get_all,
+    aws_api_gateway_integration.post_item,
+    aws_api_gateway_integration.get_one,
+    aws_api_gateway_integration.delete_item,
+    aws_api_gateway_integration.patch_item
+  ]
+}
+
+resource "aws_api_gateway_stage" "dev" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.my_api.id
+  stage_name    = "dev"
 }
--- lambda.tf
+++ lambda.tf
@@ -1,39 +1,58 @@
-resource "aws_iam_role" "lambda_exec_role" {
-  name = "nba_api_lambda_role"
-  description = "Grants lambda CRUD access to DynamoDB and CloudWatch logging."
 
-  assume_role_policy = jsondecode({
-    Version = "2012-12-17"
+# -- IAM TRUST policy --
+resource "aws_iam_role" "lambda_exec_role" {
+  name        = "nba_api_lambda_role"
+  description = "Grants Lambda access to DynamoDB and CloudWatch logs."
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
-        {
-            Action = "sts:AssumeRole"
-            Principal = {
-                Service = "lambda.amazonaws.com"
-            }
-            Sid = "DynamoDBCrudAccess"
-            Effect = "Allow"
-            Action = [
-                "dynamodb:GetItem",
-                "dynamodb:Scan",
-                "dynamodb:PutItem",
-                "dynamodb:UpdateItem",
-                "dynamodb:DeleteItem"
-            ]
-            Resource = "arn:aws:dynamodb:eu-west-2:098597569789:table/nba-player-data"
-        },
-        {
-            Sid = "CloudWatchLogsAccess"
-            Effect = "Allow"
-            Action = [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents"
-            ]
-            Resource = "arn:aws:logs:eu-west-2:098597569789:log-group:/aws/lambda/api_processor:*"
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
         }
+        Action = "sts:AssumeRole"
+      }
     ]
   })
 }
+
+# -- IAM PERMISSIONS policy
+
+resource "aws_iam_role_policy" "lambda_permissions" {
+  name = "nba_api_lambda_policy"
+  role = aws_iam_role.lambda_exec_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "DynamoDBCrudAccess"
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:Scan",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:eu-west-2:098597569789:table/nba-player-data"
+      },
+      {
+        Sid = "CloudWatchLogsAccess"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:eu-west-2:098597569789:log-group:/aws/lambda/api_processor:*"
+      }
+    ]
+  })
+}
+
 
 resource "aws_lambda_function" "nba_api_lambda" {
   function_name = "nba_api_processor"
