f849520201e8851301de7cd2cedb92bce2ee5645
Jordan_D <jordanholstdaniels@gmail.com>
2025-12-13 01:48:40 +0000

    completed cloudwatch integration, ready for terraform application

--- iac/terraform/lambda.tf
+++ iac/terraform/lambda.tf
@@ -8,13 +8,40 @@ data "aws_iam_policy_document" "assume_role" {
       identifiers   = ["lambda.amazonaws.com"]
     }
 
-    actions = ["sts:AssumeRole"]
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+  }
+}
+
+data "aws_iam_policy_document" "lambda_cloudwatch_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = "*"
   }
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
   name                  = "lambda_execution_role"
-  assume_role_policy    = data.aws_iam_policy_document.assume_role
+  assume_role_policy    = data.aws_iam_policy_document.assume_role  
+}
+
+resource "aws_iam_policy" "lambda_logging_policy" {
+  name = "lambda_cloudwatch_logging"
+  description = "IAM policy for lambda to write logs to CloudWatch"
+  policy = data.aws_iam_policy_document.lambda_cloudwatch_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs_attach" {
+  role = aws_iam_role.iam_for_lambda.lambda_execution_role
+  policy_arn = aws_iam_policy.lambda_logging_policy.arn
 }
 
 # Package the lambda function code
