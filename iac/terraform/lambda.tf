7c91d92800f091bbee0f76ad188c0af070fac595
Jordan_D <jordanholstdaniels@gmail.com>
2025-12-14 00:42:50 +0000

    Revert "completed cloudwatch integration, ready for terraform application"
    This reverts commit f849520201e8851301de7cd2cedb92bce2ee5645.
    
    :wq

--- iac/terraform/lambda.tf
+++ iac/terraform/lambda.tf
@@ -8,40 +8,13 @@ data "aws_iam_policy_document" "assume_role" {
       identifiers   = ["lambda.amazonaws.com"]
     }
 
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-  }
-}
-
-data "aws_iam_policy_document" "lambda_cloudwatch_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-
-    resources = "*"
+    actions = ["sts:AssumeRole"]
   }
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
   name                  = "lambda_execution_role"
-  assume_role_policy    = data.aws_iam_policy_document.assume_role  
-}
-
-resource "aws_iam_policy" "lambda_logging_policy" {
-  name = "lambda_cloudwatch_logging"
-  description = "IAM policy for lambda to write logs to CloudWatch"
-  policy = data.aws_iam_policy_document.lambda_cloudwatch_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_logs_attach" {
-  role = aws_iam_role.iam_for_lambda.lambda_execution_role
-  policy_arn = aws_iam_policy.lambda_logging_policy.arn
+  assume_role_policy    = data.aws_iam_policy_document.assume_role
 }
 
 # Package the lambda function code
